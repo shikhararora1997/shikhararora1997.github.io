f2d6f71d6784d598c9c2868db6ae7c7a8d1f1615
Shikhar Arora <shikhararora@Shikhars-MacBook-Air.local>
2023-03-15 12:26:29 -0400

    added files for v1

---
+++ website_sesh.sh
@@ -0,0 +1,5 @@
+open -a "Visual Studio Code.app" /Users/sejaldua/Desktop/DESKTOP/sejaldua.github.io/index.html
+open -a "Visual Studio Code.app" /Users/sejaldua/Desktop/DESKTOP/sejaldua.github.io/projects.html
+open -a "Visual Studio Code.app" /Users/sejaldua/Desktop/DESKTOP/sejaldua.github.io/articles.html
+open -a "Visual Studio Code.app" /Users/sejaldua/Desktop/DESKTOP/sejaldua.github.io/timeline.html
+open -a "Visual Studio Code.app" /Users/sejaldua/Desktop/DESKTOP/sejaldua.github.io/bucketlist.html
